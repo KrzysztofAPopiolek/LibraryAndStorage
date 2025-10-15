b5d13ff313e5dda55f6b6f32dcdcb5b09a0ffa05
Krzysztof <kapopiolek@gmail.com>
2025-10-15 15:50:08 +0200

    StarterVersion
    starter version

---
+++ src/main/java/pl/libraryandstorage/LibraryAndStorage/LibraryAndStorageApplication.java
@@ -0,0 +1,13 @@
+package pl.libraryandstorage.LibraryAndStorage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibraryAndStorageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LibraryAndStorageApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/libraryandstorage/LibraryAndStorage/LibraryAndStorageApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.libraryandstorage.LibraryAndStorage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibraryAndStorageApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
